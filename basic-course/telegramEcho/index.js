4bf7a8e50d72a3ee0fdfe8c5e9c4ee6c9a3638bc
Yevhenii Yesafiev <yesaf.eugene@gmail.com>
2022-11-13 19:11:58 +0200

    Added console log for user message

--- basic-course/telegramEcho/index.js
+++ basic-course/telegramEcho/index.js
@@ -3,15 +3,16 @@ const axios = require('axios');
 
 require('dotenv').config();
 
-const bot = new TgBot(process.env.BOT_TOKEN, {polling: true});
+const bot = new TgBot(process.env.BOT_TOKEN, { polling: true });
 
-bot.on("message", (msg) => {
-    bot.sendMessage(msg.chat.id, msg.text);
+bot.on('message', (msg) => {
+    console.log(`User ${msg.from.username} sent message: ${msg.text}`);
+    bot.sendMessage(msg.chat.id, 'You wrote: "' + msg.text + '"');
 });
 
 bot.onText(/photo/, (msg) => {
     axios.get('https://picsum.photos/200/300')
         .then(response => {
             bot.sendPhoto(msg.chat.id, response.request.res.responseUrl);
-        })
+        });
 });
